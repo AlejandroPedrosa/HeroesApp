1caf448f14ee89e7596f28e6192b3463f77dd4df
Alejandro Pedrosa <alejandro.pedrosa@hotmail.com>
2022-02-22 17:46:02 -0300

    Authcontext y authreducer

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const NavBar = () => {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const handleLogout = () => {
     navigate("/login", { replace: true });
@@ -44,7 +46,7 @@ export const NavBar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Alejandro</span>
+          <span className="nav-item nav-link text-info">{user.name}</span>
           <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
           </button>
